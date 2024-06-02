1b88ef92b99ae2b49477d700b9001fca38f0c066
anis2566 <anichuranis1000@gmail.com>
2024-06-02 18:11:12 +0600

    fafa

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const isProtectedRoute = createRouteMatcher([
+  // '/dashboard(.*)',
+  "/seller(.*)",
+  "/sign-up/seller",
+  "/support/seller",
+  "/account"
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    const role = auth().sessionClaims?.role
+    const status = auth().sessionClaims?.status
+    if (!auth().userId) {
+      auth().protect()
+    } else {
+      if (req.nextUrl.pathname.startsWith("/seller") && role !== "seller") {
+        return NextResponse.redirect(new URL("/sign-up/seller", req.url));
+      }
+
+      // if (req.nextUrl.pathname.startsWith("/dashboard") && role !== "admin") {
+      //   return NextResponse.redirect(new URL("/", req.url));
+      // }
+    }
+  }
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
